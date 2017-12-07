790f4ea5b31499410d0c12e0d9cf66e2fec08a88
Takashi Sendo <takashi.sendo@gmail.com>
2017-12-07 23:07:45 +0900

    Change vignetts

--- vignettes/far.R
+++ vignettes/far.R
@@ -1,13 +1,30 @@
-## ----setup, include = FALSE----------------------------------------------
-knitr::opts_chunk$set(
-  collapse = TRUE,
-  comment = "#>"
-)
+## ----setenv, echo=FALSE, message=FALSE-----------------------------------
+library(far)
 
-## ---- fig.show='hold'----------------------------------------------------
-plot(1:10)
-plot(10:1)
+## ----sample2013_data-----------------------------------------------------
+fars_2013_fn <- make_filename(2013)
+fars_2013 <- fars_read(fars_2013_fn) 
+dim(fars_2013)
+fars_2013
 
-## ---- echo=FALSE, results='asis'-----------------------------------------
-knitr::kable(head(mtcars, 10))
+## ----make_filename-------------------------------------------------------
+fars_2013_fn <- make_filename(2013)
+fars_2013_fn
+
+## ----single_year---------------------------------------------------------
+fars_2014 <- fars_read_years(years = 2014)
+fars_2014[[1]]
+
+## ----multiple_years------------------------------------------------------
+fars_3yrs <- fars_read_years(years = 2013:2015)
+fars_3yrs
+
+## ----summarize_data------------------------------------------------------
+fars_summary <- fars_summarize_years(2013:2015)
+fars_summary
+
+## ----mapping_crashes-----------------------------------------------------
+library(mapdata)
+fars_map_state(21, 2014)
+fars_map_state(37, 2014)
 
